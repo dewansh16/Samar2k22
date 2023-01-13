c9468b6fe83bffe7c43128775069ac9dee804d63
Ekant-Sahu <61657506+Ekant-Sahu@users.noreply.github.com>
2023-01-13 11:19:48 +0530

    event

--- src/routes/event/event.component.jsx
+++ src/routes/event/event.component.jsx
@@ -1,13 +1,25 @@
 import BasketBallImg from "../../Assets/Images/basketBallImg.png";
-
+import "./style.css"
 function Event() {
     return (
-        <div>
-            <h1>Event Page rendered</h1>
-            <img
-                alt="basketBallImage"
-                src={BasketBallImg}
-            />
+        <div className="base">
+            <div className="event">Event</div>
+            <div className="event" id="att">Major Attractoins</div>
+            <div className="image">
+                <div><img src={BasketBallImg} alt="" /></div>
+                <div><img src={BasketBallImg} alt="" /></div>
+            </div>
+            <div class="section">INTERBRACH SPORTS</div>
+            <div className="games">
+                <div className="left"><p>carron</p></div>
+                <div className="right"><p>kabaddi</p></div>
+            </div>
+            <div className="other">OTHERS</div>
+            <div className="oevent">
+                <div className="right">night camping</div>
+                <div className="left">archery</div>
+            </div>
+            <div>REGISTER FOR EVENT</div>
         </div>
     )
 }

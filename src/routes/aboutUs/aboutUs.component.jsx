520b86cd9f5ca84b3c1f7e3fe7c20014dc0dd163
Dewansh Patle <dewanshpatle69@gmail.com>
2023-01-14 20:57:34 +0530

    context added for navbar

--- src/routes/aboutUs/aboutUs.component.jsx
+++ src/routes/aboutUs/aboutUs.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './aboutUs.component.css'
 
 function AboutUs() {
     return (
-        <div>
+        <div className="">
             <h1>AboutUs Page rendered</h1>
         </div>
     )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom"
-import React, { useState } from 'react';
+import { Outlet} from "react-router-dom"
+import React, { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import Burger from './burger.component';
 import RightNav from "./rightNav.component";
 import Footer from "../../Components/footer/footer.component";
+import './navigation.component.css';
+
+import SamarImg from "../../Assets/Images/samar21 (1).png";
 
 const Nav = styled.nav`
   height: 70px;
@@ -33,19 +36,40 @@ min-height: 100%;
 width: 100%;
 `
 
+
 function Navigation() {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        },2000)
+    },[])
+
     return (
-        <BodyDiv>
-            <NavDiv>
-                <Burger open={open} setOpen={setOpen}/>
-                <Nav>
-                    <RightNav open={open} setOpen={setOpen}/>
-                </Nav>
-            </NavDiv>
-            <Outlet />
-            <Footer/>
-        </BodyDiv>
+        <>
+            <div style={isLoading?{}:{display:'none'}} className="animationDiv">
+                <div>
+                    <img
+                        alt="samarImage"
+                        src={SamarImg}
+                    />
+                </div>
+            </div>
+            <BodyDiv>
+                <NavDiv>
+                    <Burger open={open} setOpen={setOpen}/>
+                    <Nav>
+                        <RightNav open={open} setOpen={setOpen}/>
+                    </Nav>
+                </NavDiv>
+                <Outlet />
+                <Footer/>
+            </BodyDiv>
+        </>
     )
 }
 

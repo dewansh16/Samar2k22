db25a12a491824bcf932875a4b93b29b9081037f
Neha Sahu <nehasahu160802@gmail.com>
2023-01-15 22:46:10 +0530

    added sponsors gallery

--- src/routes/sponsors/sponsors.component.jsx
+++ src/routes/sponsors/sponsors.component.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+import "./style1.css";
+import sponsor from "../../Assets/Images/sponsors.jpg";
+
 
 function Sponsors() {
-    return (
-        <div>
-            <h1>Sponsors Page rendered</h1>
-        </div>
-    )
+  return (
+    <div>
+      
+      <div className="container">
+        <div className="section1">
+            
+        <div className="heading">
+          <p>SPONSORS</p> </div>
+        
+        </div> 
+      </div>
+
+      <div className="sponsimage">
+      <img 
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            /> 
+
+     <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />   
+     <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />   
+
+      <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />         
+      
+
+      <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            /> 
+
+     <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />   
+     <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />   
+
+      
+   
+
+      <img
+                alt="chess"
+                src={sponsor}
+                className = "img1"
+            />   
+            
+            </div>
+    </div>
+  );
 }
 
-export default Sponsors
+export default Sponsors;

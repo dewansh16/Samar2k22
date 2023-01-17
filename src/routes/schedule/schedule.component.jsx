46bdbb42cbcb91b19427524e94dd992fb52f324a
Dewansh Patle <dewanshpatle69@gmail.com>
2023-01-17 20:39:20 +0530

    added Ekant's code and bug fixes

--- src/routes/schedule/schedule.component.jsx
+++ src/routes/schedule/schedule.component.jsx
@@ -1,11 +1,148 @@
 import React from 'react'
+import "./style2.css"
+
 
 function Schedule() {
     return (
-        <div>
-            <h1>Schedule Page rendered</h1>
+        <div className='base'>
+            <div className="event">SCHEDULE</div>
+            <div className="day">DAY 1</div>
+            <div className="games">
+{/* comment */}
+
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+
+{/* comment */}
+            </div>
+            <div className="day">DAY 1</div>
+            <div className="games">
+{/* comment */}
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>  
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+
+{/* comment */}
+            </div>
+            <div className="day">DAY 1</div>
+            <div className="games">
+{/* comment */}
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+
+{/* comment */}
+            </div>
+            <div className="day">DAY 1</div>
+            <div className="games">
+{/* comment */}
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+                <div className='flex-container'>
+                            <div className='left'>basketball</div>
+                            <div className='right'>9:00 AM - 10:00 AM</div>      
+                </div>
+
+            </div>
         </div>
     )
 }
-
 export default Schedule

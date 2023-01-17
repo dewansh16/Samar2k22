8361bee0d5c97947faa4ef3bf6041c792e05a9a7
Dewansh Patle <dewanshpatle69@gmail.com>
2023-01-17 20:55:02 +0530

    changed video link in homePage

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -70,7 +70,7 @@ function Home() {
 
             {/* Samar Youtube */}
             <div className="embadded-video">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/H2OSghiMRaM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/HLTSKIlLRPE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             
             {/* speakers, schedule, sponsers, gallary */}

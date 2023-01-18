a87f06aea05d39914db43b6443bb3b0e8e3f213b
Neha Sahu <nehasahu160802@gmail.com>
2023-01-19 00:09:36 +0530

    added spons page

--- src/routes/sponsors/sponsors.component.jsx
+++ src/routes/sponsors/sponsors.component.jsx
@@ -1,73 +1,53 @@
 import React from "react";
 import "./style1.css";
 import sponsor from "../../Assets/Images/sponsors.jpg";
-
-
+import dainikBhaskar from "../../Assets/Images/dainikbhaskar.jpg";
+import ibc from "../../Assets/Images/ibc.jpg";
 function Sponsors() {
   return (
-    <div>
-      
-      <div className="container">
-        <div className="section1">
-            
-        <div className="heading">
-          <p>SPONSORS</p> </div>
-        
-        </div> 
+    <div class="spo-wrapper">
+      <div class="spo-sidebar">
+        <h1 className="spo-heading1">SPONSORS</h1>
+        <p  className="spo-text" >
+          SAMAR success is unmatched and this
+          is due to renowned partners we have who have always provided
+          unequivocal support . The mutual work between samar and it's partners
+          have always lived up to the expectations of the audience and attracted
+          a new audience.
+        </p>
       </div>
+      <div class="spo-main">
+        <div className="sponsor-first">
+          <h1 className="spo-heading" >PRINT MEDIA PARTENER</h1>
+            <img src={dainikBhaskar} alt="dainikbhaskar" className="spo-img" />
+        </div>
 
-      <div className="sponsimage">
-      <img 
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            /> 
+        <div className="sponsor-first">
+          <h1 className="spo-heading">TELEVISION MEDIA PARTENER</h1>
+            <img src={ibc} alt="ibc24" className="spo-img" />
+        </div>
 
-     <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />   
-     <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />   
+        <div className="sponsor-first">
+          <h1 className="spo-heading">PRINT MEDIA PARTENER</h1>
+            <img src={dainikBhaskar} alt="dainikbhaskar" className="spo-img" />
+        </div>
 
-      <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />         
-      
+        <div className="sponsor-first">
+          <h1 className="spo-heading">TELEVISION MEDIA PARTENER</h1>
+            <img src={ibc} alt="ibc24" className="spo-img" />
+        </div>
+        
+        <div className="sponsor-first">
+          <h1 className="spo-heading">PRINT MEDIA PARTENER</h1>
+            <img src={dainikBhaskar} alt="dainikbhaskar" className="spo-img" />
+        </div>
 
-      <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            /> 
+        <div className="sponsor-first">
+          <h1 className="spo-heading">TELEVISION MEDIA PARTENER</h1>
+            <img src={ibc} alt="ibc24" className="spo-img" />
+        </div>
 
-     <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />   
-     <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />   
-
-      
-   
-
-      <img
-                alt="chess"
-                src={sponsor}
-                className = "img1"
-            />   
-            
-            </div>
+      </div>
     </div>
   );
 }

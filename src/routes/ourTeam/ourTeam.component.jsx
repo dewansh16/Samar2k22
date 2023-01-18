1a00c2345ec694297594aec0d7f4d989c08a4072
Dewansh Patle <dewanshpatle69@gmail.com>
2023-01-19 01:51:52 +0530

    our team page added

--- src/routes/ourTeam/ourTeam.component.jsx
+++ src/routes/ourTeam/ourTeam.component.jsx
@@ -7,182 +7,187 @@ function ourTeam() {
       <p className="team-head-text">OUR TEAM</p>
       <p className="overall" id="overall">OVERALL</p>
 
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Rajat Pradhan</h2>
-            <p class="copy">3rd Biomed</p>
-            <p class="copy">+91-987654310</p>
-
+      <main className="page-content">
+        <div className = "card-container">
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Rajat Pradhan</h2>
+              <p className="copy">3rd Biomed</p>
+              <p className="copy">+91-987654310</p>
+            </div>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className = "card-container">
+          <div className="card">
+            <div className="content">
+              <h2 className="title">To The Beach</h2>
+              <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+            </div>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className = "card-container">
+          <div className="card">
+            <div className="content">
+              <h2 className="title">To The Beach</h2>
+              <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+            </div>
           </div>
         </div>
       </main>
 
       <p className="overall" id="spons">SPONSORSHIP</p>
 
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Anupam Banjare</h2>
-            <p class="copy">8th | Meta</p>
-            <p class="copy">+91-8109785733</p>
+      <main className="page-content">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Anupam Banjare</h2>
+            <p className="copy">8th | Meta</p>
+            <p className="copy">+91-8109785733</p>
 
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Desert Destinations</h2>
-            <p class="copy">It's the desert you've always dreamed of</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Desert Destinations</h2>
+            <p className="copy">It's the desert you've always dreamed of</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Explore The Galaxy</h2>
-            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Explore The Galaxy</h2>
+            <p className="copy">Seriously, straight up, just blast off into outer space today</p>
           </div>
         </div>
       </main>
 
       <p className="overall" id="design">DESIGN</p>
 
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <main className="page-content">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">To The Beach</h2>
+            <p className="copy">Plan your next beach trip with these fabulous destinations</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Desert Destinations</h2>
-            <p class="copy">It's the desert you've always dreamed of</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Desert Destinations</h2>
+            <p className="copy">It's the desert you've always dreamed of</p>
           </div>
         </div>
       </main>
 
       <p className="overall" >SPORTS COORDINATOR</p>
 
-      <main class="page-content" id="sc">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <main className="page-content" id="sc">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">To The Beach</h2>
+            <p className="copy">Plan your next beach trip with these fabulous destinations</p>
           </div>
         </div>
       </main>
 
       <p className="overall" id="pr">PR & Marketing</p>
 
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <main className="page-content">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">To The Beach</h2>
+            <p className="copy">Plan your next beach trip with these fabulous destinations</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Desert Destinations</h2>
-            <p class="copy">It's the desert you've always dreamed of</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Desert Destinations</h2>
+            <p className="copy">It's the desert you've always dreamed of</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Explore The Galaxy</h2>
-            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Explore The Galaxy</h2>
+            <p className="copy">Seriously, straight up, just blast off into outer space today</p>
           </div>
         </div>
       </main>
 
       <p className="overall" id="discipline">DISCIPLINE</p>
 
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <main className="page-content">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">To The Beach</h2>
+            <p className="copy">Plan your next beach trip with these fabulous destinations</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Desert Destinations</h2>
-            <p class="copy">It's the desert you've always dreamed of</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Desert Destinations</h2>
+            <p className="copy">It's the desert you've always dreamed of</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Explore The Galaxy</h2>
-            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Explore The Galaxy</h2>
+            <p className="copy">Seriously, straight up, just blast off into outer space today</p>
           </div>
         </div>
 
       </main>
       
-      <main class="page-content">
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Mountain View</h2>
-            <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+      <main className="page-content">
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Mountain View</h2>
+            <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">To The Beach</h2>
-            <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">To The Beach</h2>
+            <p className="copy">Plan your next beach trip with these fabulous destinations</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Desert Destinations</h2>
-            <p class="copy">It's the desert you've always dreamed of</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Desert Destinations</h2>
+            <p className="copy">It's the desert you've always dreamed of</p>
           </div>
         </div>
-        <div class="card">
-          <div class="content">
-            <h2 class="title">Explore The Galaxy</h2>
-            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Explore The Galaxy</h2>
+            <p className="copy">Seriously, straight up, just blast off into outer space today</p>
           </div>
         </div>
       </main>

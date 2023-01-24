a974916df9a1daa7fdeb0ff69390aae971d16095
Dewansh Patle <dewanshpatle69@gmail.com>
2023-01-24 12:07:56 +0530

    added shaurya logo in header for mobile view

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,20 @@
-import { Outlet} from "react-router-dom"
+import { useContext } from "react";
+import { Outlet } from "react-router-dom"
 import React, { useState,useEffect } from 'react';
+import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import Burger from './burger.component';
 import RightNav from "./rightNav.component";
 import Footer from "../../Components/footer/footer.component";
+import { NavbarContext } from "../../contexts/navbar.context";
 import './navigation.component.css';
 import LandingLoader from "../../Components/animationPage/landingLoader.component";
+import headLogo from '../../Assets/Images/samar_logo.png';
 
 const Nav = styled.nav`
   height: 90px;
   font-family: GOTSamar;
-position:fixed; 
+  position:fixed; 
   top:0;
   left:0;
   width:100%;
@@ -23,7 +27,7 @@ position:fixed;
   -webkit-backdrop-filter: blur(6.5px);
   display: flex;
   justify-content: space-between;
-  @media (max-width: 800px) {
+  @media (max-width: 1000px) {
       position: absolute;
       backdrop-filter:none;
       background:none;
@@ -46,6 +50,10 @@ width: 100%;
 function Navigation() {
     const [open, setOpen] = useState(false);
     const [isLoading, setLoading] = useState(false);
+
+
+    const { activeLink, setActiveLink } = useContext(NavbarContext);
+
     useEffect(() => {
         setLoading(true);
         setTimeout(() => {
@@ -54,9 +62,21 @@ function Navigation() {
     },[])
     return (
         <>
-            {isLoading ?<LandingLoader isLoading={isLoading} />:
+            {isLoading ? <LandingLoader isLoading={isLoading} /> :
                 <BodyDiv>
                     <NavDiv>
+                        <div className="shaurya-head-div">
+                            <Link
+                            style={{position:'fixed',left:'4%',top:'15px'}}
+                            to="/"
+                            onClick={() => {
+                            setActiveLink(0);
+                            setOpen(false);
+                                }}
+                            >
+                                <img src={headLogo} className="head-logo-img" alt="Samar logo" />
+                            </Link>
+                        </div>
                         <Burger open={open} setOpen={setOpen}/>
                         <Nav>
                             <RightNav open={open} setOpen={setOpen}/>

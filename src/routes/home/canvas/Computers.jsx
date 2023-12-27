40cc24c8298793216d3d49854fd8104aec031470
AKSHAY JAIN <akshay.070603@gmail.com>
2023-12-27 07:52:32 +0530

    15th commit

--- src/routes/home/canvas/Computers.jsx
+++ src/routes/home/canvas/Computers.jsx
@@ -6,26 +6,25 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./lucario/scene.gltf");
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      {/* <hemisphereLight intensity={0.15} groundColor='black' /> */}
+      <hemisphereLight intensity={3} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={3}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={3} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 2 : 2.5}
+        scale={isMobile ? 2.2 : 2.5}
         // scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [-9, -4, -2.2] : [0, -2, -1.5]}
+        // position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, 1.2, -0.1]}
         // rotation={[-0.01, -0.2, -0.1]}
       />
@@ -38,6 +37,7 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
+    // const mediaQuery = window.matchMedia("(max-width: 700px)");
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
     // Set the initial value of the `isMobile` state variable

3c5e9fa56e6ae92d682e9ad0c2890ece13be8ef9
AKSHAY JAIN <akshay.070603@gmail.com>
2023-12-27 07:17:35 +0530

    14th commit

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -24,7 +24,7 @@ import team9 from "../../Assets/Images/playing-teams/team9.jpg";
 import { motion } from "framer-motion";
 
 import { styles } from "../../styles";
-// import { ComputersCanvas } from "./canvas";
+import { ComputersCanvas } from "./canvas";
 
 
 
@@ -171,8 +171,8 @@ function Home() {
   return (
     <>
     {/* <div className="fireAnimation"> */}
-     <div id="progressBar" style={scrollbarStyleRight}></div>
-     <div id="progressBar" style={scrollbarStyleLeft}></div>
+     {/* <div id="progressBar" style={scrollbarStyleRight}></div> */}
+     {/* <div id="progressBar" style={scrollbarStyleLeft}></div> */}
 
 
 
@@ -226,7 +226,7 @@ function Home() {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-[150px] bottom-[150px] w-full flex justify-center items-center'>
       {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'> */}

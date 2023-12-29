ab2e2b953ae4ffd03ef18d9219ce6f257adb82d3
AKSHAY JAIN <akshay.070603@gmail.com>
2023-12-30 01:01:26 +0530

    twitter logo and padding fixed for first page

--- src/Assets/Icons/index.js
+++ src/Assets/Icons/index.js
@@ -1,6 +1,6 @@
 import linkedIn from "./linkedin1.svg"
 import instagram from "./instagram1.svg"
-import twitter from "./twitter1.svg"
+import twitter from "./twitterX.svg"
 
 export {
     linkedIn,
--- src/routes/home/FirstPart.jsx
+++ src/routes/home/FirstPart.jsx
@@ -16,7 +16,7 @@ const FirstPart = () => {
 
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white pr-10`}>
             Team <span className='text-[#915EFF]'>SHAURYA</span>
           </h1>
           <p className={`${styles.heroSubText} text-white-100`}>

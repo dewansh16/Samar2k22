1d65a5c06fec131087063dbd5ee156e36d315fa7
AKSHAY JAIN <akshay.070603@gmail.com>
2023-12-29 17:31:36 +0530

    margin-top fixed

--- src/routes/home/FirstPart.jsx
+++ src/routes/home/FirstPart.jsx
@@ -6,10 +6,11 @@ const FirstPart = () => {
   return (
      <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[100px] lg:top-[40px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        // className={`absolute inset-0 max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[40px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-5 h-5 mt-10 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
